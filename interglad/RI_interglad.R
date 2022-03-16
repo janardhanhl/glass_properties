5e4560127ebad8025549a9ec1912a2433772a774
janardhan <janardhanhl@gmail.com>
2022-03-16 16:54:49 +0530

    plotted RI distribution

--- interglad/RI_interglad.R
+++ interglad/RI_interglad.R
@@ -1,1 +1,7 @@
 #RI for interglad glass 
+RI_interglad<-read.csv("RI_interglad.csv")
+View(RI_interglad)
+attach(RI_interglad)
+plot(RI, main = " complete RI distribution")
+plot(RI, PbO)
+plot(RI, SiO2)
